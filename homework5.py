472979e71562c18527c11d3949933128c1b90262
MuxaJlbl417 <sudakov170389@gmail.com>
2024-05-21 10:58:22 +0500

    Initial commit

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi', 'coconut']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'peach'
+print(my_list)
+
+my_dict = {'apple':'яблоко', 'banana': 'банан', 'orange': 'апельсин', 'kiwi': 'киви'}
+print(my_dict)
+print(my_dict['orange'])
+my_dict['coconut'] = 'кокос'
+print(my_dict)
